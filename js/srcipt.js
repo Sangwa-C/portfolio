3933138ac40c878622c222f008621050c431a268
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-11-18 18:39:35 +0200

    edits on the whole page

---
+++ js/srcipt.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#img1").mouseover(function(){
+    $("#p1").show();
+  }); 
+$("#img1").mouseleave(function(){
+    $("#p1").hide();
+  });  
+$("#img2").mouseenter(function(){
+    $("#p2").show();
+  }); 
+$("#img2").mouseleave(function(){
+    $("#p2").hide();
+  });
+$("#img3").mouseenter(function(){
+    $("#p3").show();
+  }); 
+$("#img3").mouseleave(function(){
+    $("#p3").hide();
+  });  
+$("#img4").mouseenter(function(){
+    $("#p4").show();
+  }); 
+$("#img4").mouseleave(function(){
+    $("#p4").hide();
+  });
+});
